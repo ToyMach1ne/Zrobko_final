9d1f7dfbe9f39f16fcb50c8cc8f3917be8472e80
ToyMach1ne <forvinwolves@gmail.com>
2018-01-23 18:38:20 +0200

    first commit

---
+++ html/js/scripts.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".owl-carousel").owlCarousel({autoplay:!0,items:1})}),$(".items__grid--wrap").not(":first").hide(),$(".items__list--wrap .category__list--item").click(function(){$(".items__list--wrap .category__list--item").removeClass("list__item--active").eq($(this).index()).addClass("list__item--active"),$(".items__grid--wrap").hide().eq($(this).index()).fadeIn()}).eq(0).addClass("list__item--active");
+//# sourceMappingURL=maps/scripts.js.map
